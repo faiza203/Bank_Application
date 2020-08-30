b00ab019e0acd8b46a12b98a679f24cc7eac1e56
faiza203 <fiazfaiza981@gmail.com>
2020-08-30 18:06:10 +0500

    Convert h2 to btn

--- transaction.js
+++ transaction.js
@@ -6,6 +6,10 @@ const transcationHistory = document.getElementById("transcation-history");
 const addNewTransaction = document.getElementById("add_new_transaction");
 const addNewTransactionForm = document.getElementById("add_new_transaction_form");
 const addNewTransactionAmount = document.getElementById("add_new_transaction");
-const  addTransaction = document.getElementById("add_transaction");
-const  delTransaction = document.getElementById("del_transaction");
-const  cancleTransaction = document.getElementById("cancle_transaction");
+const addTransaction = document.getElementById("add_transaction");
+const delTransaction = document.getElementById("del_transaction");
+const cancleTransaction = document.getElementById("cancle_transaction");
+
+addNewTransaction.addEventListener("click", () => {
+    addNewTransactionForm.style.display = "block";
+});
