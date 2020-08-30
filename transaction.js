879191f366b482fd8e7842cca94ef50ec2f646b5
faiza203 <fiazfaiza981@gmail.com>
2020-08-30 17:58:46 +0500

    Select elements in transaction.js

---
+++ transaction.js
@@ -0,0 +1,11 @@
+// Elements
+const loadingDiv = document.getElementById("loading");
+const contentDiv = document.getElementById("content");
+const bankBalanceAmount = document.getElementById("bank-banalce-amount");
+const transcationHistory = document.getElementById("transcation-history");
+const addNewTransaction = document.getElementById("add_new_transaction");
+const addNewTransactionForm = document.getElementById("add_new_transaction_form");
+const addNewTransactionAmount = document.getElementById("add_new_transaction");
+const  addTransaction = document.getElementById("add_transaction");
+const  delTransaction = document.getElementById("del_transaction");
+const  cancleTransaction = document.getElementById("cancle_transaction");
