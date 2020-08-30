4efa9cdc5451d743d496437994bdc418fbd1dd89
faiza203 <fiazfaiza981@gmail.com>
2020-08-30 18:13:06 +0500

    Initialize Firebase

--- transaction.js
+++ transaction.js
@@ -13,3 +13,19 @@ const cancleTransaction = document.getElementById("cancle_transaction");
 addNewTransaction.addEventListener("click", () => {
     addNewTransactionForm.style.display = "block";
 });
+
+// Initialize Firebase
+(function () {
+    const config = {
+        apiKey: "AIzaSyAZX5VgMVNTyz-J_UUoCFuXIFK48pk7mzU",
+        authDomain: "common-50c43.firebaseapp.com",
+        databaseURL: "https://common-50c43.firebaseio.com",
+        projectId: "common-50c43",
+        storageBucket: "common-50c43.appspot.com",
+        messagingSenderId: "670911494230",
+        appId: "1:670911494230:web:ecefbb05d31dc80d75a3f7",
+        measurementId: "G-VBPMYERL4D"
+    };
+    firebase.initializeApp(config);
+    firebase.analytics();
+}())
